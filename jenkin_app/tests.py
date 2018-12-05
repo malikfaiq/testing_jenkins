c6f0608a7fc6a2a112ab46c860c3eaa7d6523712
malikfaiq <malikfaiq44@hotmail.com>
2018-12-05 12:43:45 +0500

    change file

--- jenkin_app/tests.py
+++ jenkin_app/tests.py
@@ -5,19 +5,11 @@ from django.urls import reverse
 class StartAppTests(TestCase):
 
     def test_start_app__correct_response(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
         response = self.client.get(reverse("start_app"))
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.content, b"hello_jenkins")
 
     def test_start_app_wrong_response(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
         response = self.client.get(reverse("start_app"))
         self.assertEquals(response.status_code, 200)
-        self.assertNotEquals(response.content, b"hello_jen")
+        self.assertEquals(response.content, b"hello_jen")
